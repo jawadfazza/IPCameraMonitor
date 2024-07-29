b04e2cf6fff20da6c204b69a0315a19b7ac310f0
Jawad <Jawad@DESKTOP-PTJS7RG>
2024-07-30 00:41:28 +0300

    #

--- IPCameraMonitor/Form1.cs
+++ IPCameraMonitor/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -32,7 +33,7 @@ namespace IPCameraMonitor
 
         private async void btnAddCamera_Click(object sender, EventArgs e)
         {
-            AddCameraPanelAsync();
+            await AddCameraPanelAsync();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -48,6 +49,7 @@ namespace IPCameraMonitor
 
             GroupBox cameraGroupBox = new GroupBox
             {
+                Text = config != null ? config.Nickname : "",
                 Name = config != null ? config.Nickname : "",
                 Size = new Size(groupBoxWidth, 350),
                 Padding = new Padding(10),
@@ -59,19 +61,49 @@ namespace IPCameraMonitor
 
             Panel actionPanel = new Panel
             {
-                Size = new Size(groupBoxWidth - 20, 40),
-                BorderStyle = BorderStyle.FixedSingle,
+                Size = new Size(groupBoxWidth - 20, 30),
+                BorderStyle = BorderStyle.None,
                 Padding = new Padding(5),
                 Dock = DockStyle.Top,
+                BackColor = Color.LightGray
+            };
+
+            // Red blinking circle
+            Panel redCircle = new Panel
+            {
+                Size = new Size(15, 15),
+                BackColor = Color.Red,
+                Visible = false,
+                Location = new Point(actionPanel.Width - 20, 7),
+                Anchor = AnchorStyles.Right
+            };
+            redCircle.Paint += (s, e) =>
+            {
+                e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                using (Brush brush = new SolidBrush(redCircle.BackColor))
+                {
+                    e.Graphics.FillEllipse(brush, 0, 0, redCircle.Width, redCircle.Height);
+                }
+            };
+
+            Timer blinkTimer = new Timer
+            {
+                Interval = 500 // Blinking interval in milliseconds
+            };
+            blinkTimer.Tick += (s, e) =>
+            {
+                redCircle.Visible = !redCircle.Visible;
             };
 
             GroupBox configPanel = new GroupBox
             {
                 Name = "configPanel",
-                Size = new Size(groupBoxWidth - 40, 200),
+                Size = new Size(groupBoxWidth - 40, 220),
                 Text = "Camera Configuration",
                 Padding = new Padding(10),
-                Dock = DockStyle.Top
+                Dock = DockStyle.Top,
+                Font = new Font("Arial", 9, FontStyle.Regular),
+                BackColor = Color.WhiteSmoke
             };
 
             configPanel.Location = new Point(
@@ -81,20 +113,21 @@ namespace IPCameraMonitor
 
             Panel cameraPanel = new Panel
             {
-                Size = new Size(groupBoxWidth - 20, 260),
+                Size = new Size(groupBoxWidth - 20, 300),
                 BorderStyle = BorderStyle.FixedSingle,
                 Padding = new Padding(10),
                 Dock = DockStyle.Fill,
+                BackColor = Color.Black
             };
 
             PictureBox pictureBox = new PictureBox
             {
                 Name = "pictureBox",
                 Dock = DockStyle.Fill,
-                BorderStyle = BorderStyle.FixedSingle,
+                BorderStyle = BorderStyle.None,
                 BackColor = Color.Black,
             };
-            pictureBox.DoubleClick += (s, e) => OpenFullScreen(pictureBox);
+
 
             Label lblIPAddress = new Label { Text = "IP Address", Location = new Point(10, 20), AutoSize = true };
             TextBox txtIPAddress = new TextBox { Name = "txtIPAddress", Size = new Size(250, 20), Location = new Point(100, 20) };
@@ -116,32 +149,42 @@ namespace IPCameraMonitor
             Label lblGroups = new Label { Text = "Groups", Location = new Point(10, 170), AutoSize = true };
             TextBox txtGroups = new TextBox { Name = "txtGroups", Size = new Size(250, 20), Location = new Point(100, 170) };
 
-            Button btnActions = new Button { Text = "Actions", Size = new Size(70, 25), Location = new Point(10, 5) };
+            Button btnActions = new Button { Text = "Actions", Size = new Size(80, 30), Location = new Point(2, 1), BackColor = Color.SteelBlue, ForeColor = Color.White };
+            Button btnUpdateConfig = new Button { Text = "Save Config", Size = new Size(110, 30), Location = new Point(82, 1), Visible = false, BackColor = Color.SteelBlue, ForeColor = Color.White };
             ContextMenuStrip actionMenu = new ContextMenuStrip();
             btnActions.ContextMenuStrip = actionMenu;
 
+            ToolStripMenuItem fullScreenMenuItem = new ToolStripMenuItem("FullScreen");
             ToolStripMenuItem connectMenuItem = new ToolStripMenuItem("Connect");
             ToolStripMenuItem disconnectMenuItem = new ToolStripMenuItem("Disconnect");
             ToolStripMenuItem recordStreamMenuItem = new ToolStripMenuItem("Record");
             ToolStripMenuItem stopRecordingMenuItem = new ToolStripMenuItem("Stop");
+            stopRecordingMenuItem.Enabled = false;
+            ToolStripMenuItem showConfigMenuItem = new ToolStripMenuItem("Show Configuration");
             ToolStripMenuItem removeMenuItem = new ToolStripMenuItem("Remove");
 
             actionMenu.Items.AddRange(new ToolStripItem[]
             {
-                connectMenuItem,
-                disconnectMenuItem,
-                recordStreamMenuItem,
-                stopRecordingMenuItem,
-                removeMenuItem
+        connectMenuItem,
+        disconnectMenuItem,
+        recordStreamMenuItem,
+        stopRecordingMenuItem,
+        
+        removeMenuItem,
+        showConfigMenuItem,
+        fullScreenMenuItem
             });
 
-            connectMenuItem.Click += async (s, e) => await ConnectCameraAsync(txtIPAddress.Text, txtUsername.Text, txtPassword.Text, comboBoxStreamType.SelectedItem.ToString(), pictureBox, connectMenuItem, disconnectMenuItem, recordStreamMenuItem, stopRecordingMenuItem, removeMenuItem, cameraPanel, configPanel);
+            connectMenuItem.Click += async (s, e) => await ConnectCameraAsync(txtIPAddress.Text, txtUsername.Text, txtPassword.Text, comboBoxStreamType.SelectedItem.ToString(), pictureBox, connectMenuItem, disconnectMenuItem, recordStreamMenuItem, stopRecordingMenuItem, showConfigMenuItem, removeMenuItem, cameraPanel, configPanel);
             disconnectMenuItem.Click += (s, e) => DisconnectCamera(txtIPAddress.Text, connectMenuItem, disconnectMenuItem, recordStreamMenuItem, stopRecordingMenuItem);
-            recordStreamMenuItem.Click += (s, e) => SaveStream(txtIPAddress.Text, comboBoxStreamType.SelectedItem.ToString(), txtUsername.Text, txtPassword.Text, recordStreamMenuItem, stopRecordingMenuItem);
-            stopRecordingMenuItem.Click += (s, e) => StopRecording(txtIPAddress.Text, recordStreamMenuItem, stopRecordingMenuItem);
+            recordStreamMenuItem.Click += (s, e) => StartRecording(txtIPAddress.Text, comboBoxStreamType.SelectedItem.ToString(), txtUsername.Text, txtPassword.Text, recordStreamMenuItem, stopRecordingMenuItem, blinkTimer, redCircle);
+            stopRecordingMenuItem.Click += (s, e) => StopRecording(txtIPAddress.Text, recordStreamMenuItem, stopRecordingMenuItem, blinkTimer, redCircle);
+            showConfigMenuItem.Click += (s, e) => ShowConfiguration(configPanel, btnUpdateConfig);
             removeMenuItem.Click += (s, e) => RemoveCameraPanel(cameraGroupBox);
 
             btnActions.Click += (s, e) => actionMenu.Show(btnActions, new Point(0, btnActions.Height));
+            btnUpdateConfig.Click += (s, e) => UpdateConfiguration(configPanel, cameraPanel, pictureBox, btnUpdateConfig);
+            fullScreenMenuItem.Click+=(s,e)=> OpenFullScreen(pictureBox);
 
             if (config != null)
             {
@@ -162,7 +205,7 @@ namespace IPCameraMonitor
                 comboBoxStreamType.SelectedItem = config.StreamType;
                 txtNickname.Text = config.Nickname;
                 txtGroups.Text = string.Join(", ", config.Groups);
-                await ConnectCameraAsync(config.IPAddress, config.Username, config.Password, config.StreamType, pictureBox, connectMenuItem, disconnectMenuItem, recordStreamMenuItem, stopRecordingMenuItem, removeMenuItem, cameraPanel, configPanel);
+                await ConnectCameraAsync(config.IPAddress, config.Username, config.Password, config.StreamType, pictureBox, connectMenuItem, disconnectMenuItem, recordStreamMenuItem, stopRecordingMenuItem, showConfigMenuItem, removeMenuItem, cameraPanel, configPanel);
             }
 
             configPanel.Controls.Add(lblIPAddress);
@@ -179,15 +222,25 @@ namespace IPCameraMonitor
             configPanel.Controls.Add(txtGroups);
 
             actionPanel.Controls.Add(btnActions);
+            actionPanel.Controls.Add(btnUpdateConfig);
+            actionPanel.Controls.Add(redCircle); // Add the red circle to the action panel
             cameraPanel.Controls.Add(pictureBox);
             cameraGroupBox.Controls.Add(actionPanel);
             cameraGroupBox.Controls.Add(configPanel);
             cameraGroupBox.Controls.Add(cameraPanel);
 
+
             flowLayoutPanel.Controls.Add(cameraGroupBox);
         }
+        private void ShowConfiguration(GroupBox configPanel, Button btnUpdateConfig)
+        {
+            configPanel.Visible = true;
+            btnUpdateConfig.Visible = true;
+        }
 
-        private async Task ConnectCameraAsync(string ipAddress, string username, string password, string streamType, PictureBox pictureBox, ToolStripMenuItem btnConnect, ToolStripMenuItem btnDisconnect, ToolStripMenuItem btnSaveStream, ToolStripMenuItem btnStopRecording, ToolStripMenuItem btnRemove, Panel cameraPanel, GroupBox configPanel)
+
+
+        private async Task ConnectCameraAsync(string ipAddress, string username, string password, string streamType, PictureBox pictureBox, ToolStripMenuItem btnConnect, ToolStripMenuItem btnDisconnect, ToolStripMenuItem btnSaveStream, ToolStripMenuItem btnStopRecording, ToolStripMenuItem btnShowConfig, ToolStripMenuItem btnRemove, Panel cameraPanel, GroupBox configPanel)
         {
             btnConnect.Enabled = false;
             btnDisconnect.Enabled = true;
@@ -229,6 +282,7 @@ namespace IPCameraMonitor
             configPanel.Visible = false;
             pictureBox.Location = new Point(10, 10);
             cameraPanel.Dock = DockStyle.Fill;
+
         }
 
         private void RemoveCameraPanel(GroupBox cameraGroupBox)
@@ -298,7 +352,7 @@ namespace IPCameraMonitor
 
         private void DisconnectCamera(string ipAddress, ToolStripMenuItem btnConnect, ToolStripMenuItem btnDisconnect, ToolStripMenuItem btnSaveStream, ToolStripMenuItem btnStopRecording)
         {
-            StopRecording(ipAddress, btnSaveStream, btnStopRecording);
+            //StopRecording(ipAddress, btnSaveStream, btnStopRecording);
 
             if (cameraButtons.ContainsKey(ipAddress))
             {
@@ -390,6 +444,70 @@ namespace IPCameraMonitor
         }
 
 
+
+        private void UpdateConfiguration(GroupBox configPanel, Panel cameraPanel, PictureBox pictureBox, Button btnUpdateConfig)
+        {
+            var txtIPAddress = configPanel.Controls["txtIPAddress"] as TextBox;
+            var txtUsername = configPanel.Controls["txtUsername"] as TextBox;
+            var txtPassword = configPanel.Controls["txtPassword"] as TextBox;
+            var comboBoxStreamType = configPanel.Controls["comboBoxStreamType"] as ComboBox;
+            var txtNickname = configPanel.Controls["txtNickname"] as TextBox;
+            var txtGroups = configPanel.Controls["txtGroups"] as TextBox;
+
+            var ipAddress = txtIPAddress.Text;
+            var username = txtUsername.Text;
+            var password = txtPassword.Text;
+            var streamType = comboBoxStreamType.SelectedItem.ToString();
+            var nickname = txtNickname.Text;
+            var groups = txtGroups.Text;
+
+            List<CameraConfig> existingConfigs = new List<CameraConfig>();
+
+            if (File.Exists(ConfigFilePath))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<CameraConfig>));
+                using (StreamReader reader = new StreamReader(ConfigFilePath))
+                {
+                    existingConfigs = (List<CameraConfig>)serializer.Deserialize(reader);
+                }
+            }
+
+            var config = existingConfigs.FirstOrDefault(c => c.IPAddress == ipAddress);
+            if (config != null)
+            {
+                config.Username = username;
+                config.Password = password;
+                config.StreamType = streamType;
+                config.Nickname = nickname;
+                config.Groups = groups.Split(',').Select(g => g.Trim()).ToList();
+            }
+            else
+            {
+                existingConfigs.Add(new CameraConfig
+                {
+                    IPAddress = ipAddress,
+                    Username = username,
+                    Password = password,
+                    StreamType = streamType,
+                    Nickname = nickname,
+                    Groups = groups.Split(',').Select(g => g.Trim()).ToList()
+                });
+            }
+
+            XmlSerializer serializerSave = new XmlSerializer(typeof(List<CameraConfig>));
+            using (StreamWriter writer = new StreamWriter(ConfigFilePath))
+            {
+                serializerSave.Serialize(writer, existingConfigs);
+            }
+
+            HideConfigControls(configPanel, cameraPanel, pictureBox);
+            LoadCameraConfigurations();
+            btnUpdateConfig.Visible=false;
+            MessageBox.Show("Configuration updated successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+      
+
         private void TreeViewConfiguredCameras_AfterSelect(object sender, TreeViewEventArgs e)
         {
             if (e.Node.Tag is CameraConfig config)
@@ -402,6 +520,7 @@ namespace IPCameraMonitor
         {
             if (File.Exists(ConfigFilePath))
             {
+                treeViewConfiguredCameras.Nodes.Clear();
                 XmlSerializer serializer = new XmlSerializer(typeof(List<CameraConfig>));
                 using (StreamReader reader = new StreamReader(ConfigFilePath))
                 {
@@ -475,7 +594,7 @@ namespace IPCameraMonitor
             recordsForm.ShowDialog();
         }
 
-        private async void SaveStream(string ipAddress, string streamType, string username, string password, ToolStripMenuItem btnSaveStream, ToolStripMenuItem btnStopRecording)
+        private async void StartRecording(string ipAddress, string streamType, string username, string password, ToolStripMenuItem btnSaveStream, ToolStripMenuItem btnStopRecording, Timer blinkTimer, Panel redCircle)
         {
             if (streamType == "MJPEG")
             {
@@ -518,12 +637,31 @@ namespace IPCameraMonitor
 
                     btnSaveStream.Enabled = false;
                     btnStopRecording.Enabled = true;
+                    // Start the blinking effect
+                    redCircle.Visible = true;
+                    blinkTimer.Start();
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show($"Error saving stream: {ex.Message}");
                     CloseVideoWriter(videoWriter);
                 }
+            }
+        }
+
+        private void StopRecording(string ipAddress, ToolStripMenuItem btnSaveStream, ToolStripMenuItem btnStopRecording, Timer blinkTimer, Panel redCircle)
+        {
+            if (videoWriters.ContainsKey(ipAddress))
+            {
+                var videoWriter = videoWriters[ipAddress];
+                CloseVideoWriter(videoWriter);
+                videoWriters.Remove(ipAddress);
+
+                btnSaveStream.Enabled = true;
+                btnStopRecording.Enabled = false;
+                // Stop the blinking effect
+                blinkTimer.Stop();
+                redCircle.Visible = false;
             }
         }
 
@@ -546,18 +684,6 @@ namespace IPCameraMonitor
             }
         }
 
-        private void StopRecording(string ipAddress, ToolStripMenuItem btnSaveStream, ToolStripMenuItem btnStopRecording)
-        {
-            if (videoWriters.ContainsKey(ipAddress))
-            {
-                var videoWriter = videoWriters[ipAddress];
-                CloseVideoWriter(videoWriter);
-                videoWriters.Remove(ipAddress);
 
-                btnSaveStream.Enabled = true;
-                btnStopRecording.Enabled = false;
-            }
-        }
     }
-
 }
